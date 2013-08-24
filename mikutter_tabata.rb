b242a2845b1cfd9d9b9307818697b957345b9efa
remia319 <tomoka319@gmail.com>
2013-08-25 02:10:34 +0900

    修正

--- mikutter_tabata.rb
+++ mikutter_tabata.rb
@@ -4,9 +4,9 @@ Plugin.create(:mikutter_tabata) do
 	command(
 		:mikutter_tabata_cmd,
 		name: '田端でバタバタ',
-		condition: -> _ { true },
+		condition: lambda{ |opt| true },
 		visible: true,
-		role: :timeline
+		role: :postbox) do |opt|e
 	) do
 		strs = "田端でバタバタ"
 		Post.primary_service.update(:message => strs)
