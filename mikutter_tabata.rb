aa40881b080a8a8e81a39bd5012aafb4b035dbe1
remia319 <tomoka319@gmail.com>
2013-08-25 02:02:41 +0900

    田端でバタバタ

---
+++ mikutter_tabata.rb
@@ -0,0 +1,15 @@
+﻿# -*- coding: utf-8 -*-
+
+Plugin.create(:mikutter_tabata) do
+	command(
+		:mikutter_tabata_cmd,
+		name: '田端でバタバタ',
+		condition: -> _ { true },
+		visible: true,
+		role: :timeline
+	) do
+		strs = "田端でバタバタ"
+		Post.primary_service.update(:message => strs)
+	end
+end
+
